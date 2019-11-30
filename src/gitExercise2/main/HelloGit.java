d61dffada5b18cf5beff4b05e9d3da231d74d522
Davide Moro <d.moro@campus.unimib.it>
2019-11-30 12:22:44 +0100

    sono il primo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("Nathan è bello");
+		System.out.println("Nathan è brutto");
 	}
 }
